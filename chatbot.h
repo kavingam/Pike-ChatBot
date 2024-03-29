87ba68e0a3788ccd40e1456a197fdb3c119a6ef7
kavingam <bhaitigam71@gmail.com>
2024-03-29 02:09:07 -1200

    update

--- chatbot.h
+++ chatbot.h
@@ -19,4 +19,21 @@ struct bot_arch {
     int key[MAX+1];
     struct bot_arch *config[MAX+1];
 };
+
+bot *search(int skey,bot *p,int *pn);
+int search_info(int skey,bot *p,int *pn);
+void printList(bot *ptr,int blank);
+/*  functions used in insertion */ 
+bot *insertByShift(int k,bot *proot);
+int rec_ins(int ikey,bot *p,int *pk,bot **pkrchild);
+void split(int k,bot *krchild,bot *p,int n,int *upkey,bot **newnode);
+/* functions used in deletion */
+bot *delete(int dkey,bot *p);
+void rec_del(int dkey,bot *p);
+void delByShift(bot *p,int n);
+int copy_succkey(bot *p,int n);
+void restore(bot *p,int n);
+void borrowLeft(bot *p,int n);
+void borrowRight(bot *p,int n);
+void combine(bot *p,int m);
 #endif
